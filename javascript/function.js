b4772b2d8c44aa9bfc235ed95cec1a80b80c5920
klifia <49423018+klifia@users.noreply.github.com>
2019-04-09 11:21:45 +0700

    Delete function.js

--- javascript/function.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript" scr="path-to-javascript-file.js"></script>
-/*effect */
-/* Demo purposes only */
-$(".hover").mouseleave(
-    function () {
-      $(this).removeClass("hover");
-    }
-  );
-/*effect */

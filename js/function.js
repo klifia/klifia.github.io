34453f18d26f99fb1686898c878f15a28d375f91
klifia <49423018+klifia@users.noreply.github.com>
2019-04-09 11:24:05 +0700

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,9 @@
+<script type="text/javascript" scr="path-to-javascript-file.js"></script>
+/*effect */
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+/*effect */
